b27680f7cad7d4a70987308a7eb362ab3b533bd7
veropra <prainoveronica@gmail.com>
2019-05-24 17:39:29 +0200

    Work in progress...milestone 2...

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var template_function = Handlebars.compile(template_html);
   //Dichiaro le variabili min e max per il limite mesi dell'anno 2018
   var min_date = '2018-01-01';
-  var max_date = '2018-12-31';
+  var max_date = '2018-12-01';
 
   var initial_date = '2018-01-01';
   var current_date = moment(initial_date);
@@ -13,7 +13,7 @@ $(document).ready(function(){
   //Intercetto il click del mese successivo
   $('#successivo').click(function(){
     if(current_date.isSameOrAfter(max_date)){
-      alert('data oltre il limite massimo');
+      alert('Non puoi andare nel 2019!');
       $(this).attr('disabled', true);
     } else {
       current_date.add(1, 'months');
@@ -22,10 +22,10 @@ $(document).ready(function(){
       $('#precedente').attr('disabled', false);
     }
   });
-
+  //Intercetto il click del mese precedente
   $('#precedente').click(function(){
     if(current_date.isSameOrBefore(min_date)){
-      alert('data oltre il limite minimo');
+      alert('Non puoi andare nel 2017!');
       $(this).attr('disabled', true);
     } else {
       current_date.subtract(1, 'months');
@@ -36,17 +36,19 @@ $(document).ready(function(){
   });
 
   function disegna_mese(current_date){
+    //Resetto il contenitore del calendario
+    $('#calendar').html('');
     //Leggo quanti giorni ci sono nel mese corrente
     var giorni = current_date.daysInMonth();
     var mese = current_date.format('MMMM');
     var anno = current_date.format('YYYY');
     //Inserisco nel titolo il mese corrente
     $('#mese_corrente').text(mese + ' ' + anno);
-
     for (var i = 1; i <= giorni; i++) {
       var giorno = i + ' ' + mese;
       var variables = {
-        'giorno_template': giorno
+        'giorno_template': giorno,
+        'single_day': current_date.format('YYYY-MM-') + i
       }
       $('#calendar').append(template_function(variables));
     }
